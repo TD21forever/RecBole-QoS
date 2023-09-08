8fb996f1e93e95154f0c5acffd3101918f647136
TD21forever <435212619@qq.com>
2023-09-08 19:19:33 +0800

    100 feat: add embedding

--- root.py
+++ root.py
@@ -8,4 +8,5 @@ ROOT_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)))
 absolute = partial(os.path.join, ROOT_DIR)
 
 ORIGINAL_DATASET_DIR = os.path.join(ROOT_DIR, 'dataset/original')
+RESOURCE_DIR = os.path.join(ROOT_DIR, "resource")
 DATASET_DIR = os.path.join(ROOT_DIR, 'dataset')
